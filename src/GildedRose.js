18f3013eb2b06472df169dd5e06a9654079f338d
Ryan Harrington <ryharrin@gmail.com>
2017-04-11 14:25:25 -0400

    refactoring. duh.

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,76 +1,102 @@
-/*
- 2006-30-84
- Leeroy was here!!
+let Item = require('./Item');
+const SULFURAS_NAME = `Sulfuras, Hand of Ragnaros`;
+const AGED_BRIE_NAME = "Aged Brie";
+const BACKSTAGE_PASS_NAME = "Backstage passes to a TAFKAL80ETC concert";
 
- Leeroy <lerooy@example.com>
- */
+const cloneItem = (item) => {
+  return new Item(item.name, item.sellIn, item.quality);
+};
 
-module.exports = {
-  updateQuality: (items) => {
-    for (var i = 0; i < items.length; i++) {
-      if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-        //TODO: Improve this code.  Word.
-        if (items[i].quality > 0) {
-          if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-            items[i].quality = items[i].quality - 1
-          }
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-          if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 6) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          //Increases the Quality of the stinky cheese if it's 11 days to due date.
-          if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              // See revision number 2394 on SVN.
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-              }
-            }
-            //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-        items[i].sellIn = items[i].sellIn - 1;
-      }
-      if (items[i].sellIn < 0) {
-        if ("Aged Brie" != items[i].name) {
-          if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-            if (items[i].quality > 0) {
-              if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-                items[i].quality = items[i].quality - 1
-              }
-            }
-          } else {
-            //TODO: Fix this.
-            items[i].quality = items[i].quality - items[i].quality
-          }
-        } else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
-          }
-          if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-            items[i].quality = 0;
-        } // of for.
-      }
-      if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-        if (items[i].quality > 50) items[i].quality = 50;
+const decreaseQuality = (item) => {
+    item.quality = item.quality - 1
+};
+
+const decreaseQualityIfNotAgedBrieOrPass = (item) => {
+    if (item.quality > 0) {
+        let newItem = cloneItem(item);
+        newItem.quality--;
+        return newItem;
     }
-    return items;
-  }
-}
+};
+let brieQualityUpdate = function (item) {
+
+        if (item.sellIn < 11) {
+            item.quality = item.quality + 1
+            if (item.sellIn < 6) {
+                item.quality = item.quality + 1
+            }
+        }
+};
+let backstagePassQualityUpdate = function (item) {
+    if (BACKSTAGE_PASS_NAME === item.name) {
+        if (item.sellIn < 11) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+        }
+        if (item.sellIn < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+        }
+    }
+};
+const increaseQualityOfBrieOrPass = (item) => {
+    if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (AGED_BRIE_NAME === item.name) {
+            brieQualityUpdate(item);
+        } else {
+            backstagePassQualityUpdate(item);
+        }
+    }
+};
+const isBrieOrPass = (item) => {
+    return AGED_BRIE_NAME !== item.name && BACKSTAGE_PASS_NAME !== item.name;
+};
+
+exports.updateQuality = (items) => {
+
+
+
+    let updatedItems = items
+        .filter(item => item.name !== SULFURAS_NAME)
+        .map((item) => {
+            if (isBrieOrPass(item)) {
+                return decreaseQualityIfNotAgedBrieOrPass(item);
+            } else {
+                increaseQualityOfBrieOrPass(item);
+                return item;
+            }
+        })
+        .map(item =>{
+        item.sellIn = item.sellIn - 1;
+
+
+        if (item.sellIn < 0) {
+            if (AGED_BRIE_NAME !== item.name) {
+                if (BACKSTAGE_PASS_NAME !== item.name && item.quality > 0) {
+                      decreaseQuality(item);
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+                if (!(item.sellIn > 0))
+                    item.quality = 0;
+            }
+        }
+        if (item.quality > 50) item.quality = 50;
+        return item;
+    });
+
+    let sulfuras = items.find(item => item.name === SULFURAS_NAME);
+
+    if(sulfuras){
+      updatedItems.push(sulfuras);
+    }
+
+    return updatedItems;
+  };
