a80e65046167030fe21003b8953e358e9092534d
Matthew Morgan <matt@dulcetsoftware.com>
2017-04-12 04:53:31 -0400

    Remove filter now that we're mapping

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -8,84 +8,86 @@
 function updateConjuredItem(item){
   item.quality-=2;
   item.sellIn--;
+  return item;
 }
 
-function isNotSulfuras(item){
-  return item.name !== 'Sulfuras, Hand of Ragnaros';
+function isSulfuras(name){
+  return name === 'Sulfuras, Hand of Ragnaros';
 }
 
 exports.updateQuality = (items) => {
-  items.filter(isNotSulfuras).forEach(item => {
-    if (item.name === 'Conjured') {
-      updateConjuredItem(item);
+  return items.map(item => {
+    let {quality, sellIn, name} = item;
+    if (isSulfuras(name)) return item;
+    if (name === 'Conjured') {
+      return updateConjuredItem(item);
     } else {
-      if ("Aged Brie" != item.name && "Backstage passes to a TAFKAL80ETC concert" != item.name) {
+      if ("Aged Brie" != name && "Backstage passes to a TAFKAL80ETC concert" != name) {
         //TODO: Improve this code.  Word.
-        if (item.quality > 0) {
-          if ("Sulfuras, Hand of Ragnaros" != item.name) {
-            item.quality = item.quality - 1
+        if (quality > 0) {
+          if ("Sulfuras, Hand of Ragnaros" != name) {
+            quality = quality - 1
           }
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if ("Aged Brie" == item.name) {
-            if (item.sellIn < 6) {
-              item.quality = item.quality + 1
+        if (quality < 50) {
+          quality = quality + 1
+          if ("Aged Brie" == name) {
+            if (sellIn < 6) {
+              quality = quality + 1
             }
           }
           //Increases the Quality of the stinky cheese if it's 11 days to due date.
-          if ("Aged Brie" == item.name) {
-            if (item.sellIn < 11) {
-              item.quality = item.quality + 1
+          if ("Aged Brie" == name) {
+            if (sellIn < 11) {
+              quality = quality + 1
             }
           }
-          if ("Backstage passes to a TAFKAL80ETC concert" == item.name) {
-            if (item.sellIn < 11) {
+          if ("Backstage passes to a TAFKAL80ETC concert" == name) {
+            if (sellIn < 11) {
               // See revision number 2394 on SVN.
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
+              if (quality < 50) {
+                quality = quality + 1
               }
             }
             //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
+            if (sellIn < 6) {
+              if (quality < 50) {
+                quality = quality + 1
               }
             }
           }
         }
       }
-      if ("Sulfuras, Hand of Ragnaros" != item.name) {
-        item.sellIn = item.sellIn - 1;
+      if ("Sulfuras, Hand of Ragnaros" != name) {
+        sellIn = sellIn - 1;
       }
-      if (item.sellIn < 0) {
-        if ("Aged Brie" != item.name) {
-          if ("Backstage passes to a TAFKAL80ETC concert" != item.name) {
-            if (item.quality > 0) {
-              if ("Sulfuras, Hand of Ragnaros" != item.name) {
-                item.quality = item.quality - 1
+      if (sellIn < 0) {
+        if ("Aged Brie" != name) {
+          if ("Backstage passes to a TAFKAL80ETC concert" != name) {
+            if (quality > 0) {
+              if ("Sulfuras, Hand of Ragnaros" != name) {
+                quality = quality - 1
               }
             }
           } else {
             //TODO: Fix this.
-            item.quality = item.quality - item.quality
+            quality = quality - quality
           }
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
+          if (quality < 50) {
+            quality = quality + 1
           }
-          if ("Aged Brie" == item.name && item.sellIn <= 0)
-            item.quality = 0;
+          if ("Aged Brie" == name && sellIn <= 0)
+            quality = 0;
         } // of for.
       }
-      if ("Sulfuras, Hand of Ragnaros" != item.name)
-        if (item.quality > 50) {
-          item.quality = 50;
+      if ("Sulfuras, Hand of Ragnaros" != name)
+        if (quality > 50) {
+          quality = 50;
         }
+      return { sellIn, name, quality };
     }
-
   })
-  return items;
 }
 
